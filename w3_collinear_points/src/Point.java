90ea5e3441d5859050512b0fb961e3d79046ef78
Pavel Lazarenko <pawlaz33@gmail.com>
2024-02-24 15:42:50 +0100

    copied main files from the example

--- w3_collinear_points/src/Point.java
+++ w3_collinear_points/src/Point.java
@@ -1,23 +1,63 @@
+/******************************************************************************
+ *  Compilation:  javac Point.java
+ *  Execution:    java Point
+ *  Dependencies: none
+ *
+ *  An immutable data type for points in the plane.
+ *  For use on Coursera, Algorithms Part I programming assignment.
+ *
+ ******************************************************************************/
+
+import edu.princeton.cs.algs4.StdDraw;
+
 import java.util.Comparator;
 
 public class Point implements Comparable<Point> {
     private final int x;
     private final int y;
 
+    /**
+     * Initializes a new point.
+     *
+     * @param x the <em>x</em>-coordinate of the point
+     * @param y the <em>y</em>-coordinate of the point
+     */
     public Point(int x, int y) {
+        /* DO NOT MODIFY */
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Draws this point to standard draw.
+     */
     public void draw() {
-    }
-
-    public void drawTo(final Point that) {
-
+        /* DO NOT MODIFY */
+        StdDraw.point(x, y);
     }
 
     /**
-     * Compare two points by y-coordinates, breaking ties by x-coordinates.
+     * Draws the line segment between this point and the specified point
+     * to standard draw.
+     *
+     * @param that the other point
+     */
+    public void drawTo(Point that) {
+        /* DO NOT MODIFY */
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    /**
+     * Compares two points by y-coordinate, breaking ties by x-coordinate.
+     * Formally, the invoking point (x0, y0) is less than the argument point
+     * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
+     *
+     * @param that the other point
+     * @return the value <tt>0</tt> if this point is equal to the argument
+     * point (x0 = x1 and y0 = y1);
+     * a negative integer if this point is less than the argument
+     * point; and a positive integer if this point is greater than the
+     * argument point
      */
     public int compareTo(final Point that) {
         if (this.y < that.y) {
@@ -37,6 +77,17 @@ public class Point implements Comparable<Point> {
         return 0;
     }
 
+    /**
+     * Returns the slope between this point and the specified point.
+     * Formally, if the two points are (x0, y0) and (x1, y1), then the slope
+     * is (y1 - y0) / (x1 - x0). For completeness, the slope is defined to be
+     * +0.0 if the line segment connecting the two points is horizontal;
+     * Double.POSITIVE_INFINITY if the line segment is vertical;
+     * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
+     *
+     * @param that the other point
+     * @return the slope between this point and the specified point
+     */
     public double slopeTo(final Point that) {
         // Treat the slope of a degenerate line segment (between a point and itself) as negative infinity.
         if (this.x == that.x && this.y == that.y) {
@@ -57,9 +108,10 @@ public class Point implements Comparable<Point> {
     }
 
     /**
-     * Compare two points by slopes they make with this point.
-     * Formally, the point (x1, y1) is less than the point (x2, y2)
-     * if and only if the slope (y1 − y0) / (x1 − x0) is less than the slope (y2 − y0) / (x2 − x0)
+     * Compares two points by the slope they make with this point.
+     * The slope is defined as in the slopeTo() method.
+     *
+     * @return the Comparator that defines this ordering on points
      */
     public Comparator<Point> slopeOrder() {
         return (p1, p2) -> {
@@ -77,11 +129,16 @@ public class Point implements Comparable<Point> {
         };
     }
 
+    /**
+     * Returns a string representation of this point.
+     * This method is provide for debugging;
+     * your program should not rely on the format of the string representation.
+     *
+     * @return a string representation of this point
+     */
     public String toString() {
-        return "Point{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
+        /* DO NOT MODIFY */
+        return "(" + x + ", " + y + ")";
     }
 
     public int getX() {

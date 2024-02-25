9841ccd7fa1fae36dde907a0fef05635413cbea9
Pavel Lazarenko <pawlaz33@gmail.com>
2024-02-25 09:04:58 +0100

    Incorrect order for points processing fixed

--- w3_collinear_points/src/FastCollinearPoints.java
+++ w3_collinear_points/src/FastCollinearPoints.java
@@ -14,6 +14,7 @@ public class FastCollinearPoints {
             throw new IllegalArgumentException("Points array is empty");
         }
 
+        // TODO: merge these steps via in-place sorting
         Point[] pCopy = new Point[points.length];
         for (int i = 0; i < points.length; i++) {
             if (points[i] == null) {
@@ -29,7 +30,7 @@ public class FastCollinearPoints {
             }
         }
 
-        calculateSegments(pCopy);
+        calculateSegments(points, pCopy);
     }
 
     public int numberOfSegments() {
@@ -45,7 +46,7 @@ public class FastCollinearPoints {
     }
 
     // The method segments() should include each line segment containing 4 points exactly once
-    private void calculateSegments(final Point[] points) {
+    private void calculateSegments(final Point[] originalPoints, final Point[] points) {
         if (points.length < 4) {
             this.lineSegments = new LineSegment[0];
             return;
@@ -53,40 +54,35 @@ public class FastCollinearPoints {
 
         // TODO: shrink after calculation
         this.lineSegments = new LineSegment[points.length * 2];
-        for (int i = 0; i < points.length; i++) {
-            Point p = points[i];
+        for (int i = 0; i < originalPoints.length; i++) {
+            Point p = originalPoints[i]; // keep points order
             Arrays.sort(points, p.slopeOrder());
 
-            int count = 0;
+            int count = 1;
             Point min = p;
             Point max = p;
-            double initSlope = Double.NEGATIVE_INFINITY;
+            double initSlope = p.slopeTo(points[1]); // 0 is our point.
             for (int j = 1; j < points.length; j++) { // TODO more than 5
                 double slope = p.slopeTo(points[j]);
-                if (Double.compare(initSlope, Double.NEGATIVE_INFINITY) == 0) {
-                    initSlope = slope;
-                }
-
                 if (Double.compare(initSlope, slope) != 0) {
-                    if (count >= 3 && p.compareTo(min) == 0) {
+                    if (count > 3 && p.compareTo(min) == 0) {
                         lineSegments[numberOfSegments++] = new LineSegment(min, max);
                     }
-                    initSlope = Double.NEGATIVE_INFINITY;
-                    count = 0;
+                    initSlope = slope;
+                    count = 1;
                     min = p;
                     max = p;
                 }
-                else {
-                    if (min.compareTo(points[j]) > 0) {
-                        min = points[j];
-                    }
-                    else if (max.compareTo(points[j]) < 0) {
-                        max = points[j];
-                    }
-                    count++;
+
+                if (min.compareTo(points[j]) > 0) {
+                    min = points[j];
                 }
+                else if (max.compareTo(points[j]) < 0) {
+                    max = points[j];
+                }
+                count++;
             }
-            if (count >= 3 && p.compareTo(min) == 0) {
+            if (count > 3 && p.compareTo(min) == 0) {
                 lineSegments[numberOfSegments++] = new LineSegment(min, max);
             }
         }

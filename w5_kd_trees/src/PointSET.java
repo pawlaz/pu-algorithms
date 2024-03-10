6ed237c7f568d0480c12e2ebf7c82436476d67cb
Pavel Lazarenko <pawlaz33@gmail.com>
2024-03-10 08:35:26 +0100

    WIP w5 empty interfaces for the task are added

---
+++ w5_kd_trees/src/PointSET.java
@@ -0,0 +1,57 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+import java.util.TreeSet;
+
+public class PointSET {
+    private final TreeSet<Point2D> points;
+
+    public PointSET() {
+        points = new TreeSet<>();
+    }
+
+    private void validateArgs(final Object... args) {
+        for (Object arg : args) {
+            if (arg == null) {
+                throw new IllegalArgumentException("The argument of the function can't be null");
+            }
+        }
+    }
+
+    public boolean isEmpty() {
+        return points.isEmpty();
+    }
+
+    public int size() {
+        return points.size();
+    }
+
+    public void insert(final Point2D p) {
+        validateArgs(p);
+        points.add(p);
+    }
+
+    public boolean contains(final Point2D p) {
+        validateArgs(p);
+        return points.contains(p);
+    }
+
+    public void draw() {
+        points.parallelStream().forEach(Point2D::draw);
+    }
+
+    public Iterable<Point2D> range(final RectHV rect) {
+        validateArgs(rect);
+        return null; // TODO
+
+    }
+
+    public Point2D nearest(final Point2D p) {
+        validateArgs(p);
+        return null; // TODO
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

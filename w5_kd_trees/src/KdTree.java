59e8f93da67487b28747ed3070cbdffc6ac74dd8
Pavel Lazarenko <pawlaz33@gmail.com>
2024-03-10 10:30:59 +0100

    Base BST implementation added to kdTree

--- w5_kd_trees/src/KdTree.java
+++ w5_kd_trees/src/KdTree.java
@@ -6,14 +6,61 @@
 
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
 
-import java.util.TreeSet;
+import java.awt.Color;
 
 public class KdTree {
-    private final TreeSet<Point2D> points;
+    private Node root = null;
+    private int size = 0;
 
-    public KdTree() {
-        points = new TreeSet<>();
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void insert(final Point2D p) {
+        validateArgs(p);
+        root = put(root, p, true);
+    }
+
+    private Node put(final Node node, final Point2D point, final boolean isVertical) {
+        if (node == null) {
+            size++;
+            return new Node(point, isVertical);
+        }
+
+        if (node.isSamePoint(point)) {
+            return node;
+        }
+
+        int cmp = node.compareTo(point);
+        if (cmp > 0) {
+            node.left = put(node.left, point, !isVertical);
+        }
+        else if (cmp < 0) {
+            node.right = put(node.right, point, !isVertical);
+        }
+
+        return node;
+    }
+
+    private Node get(final Node node, final Point2D point) {
+        if (node == null) return null;
+
+        if (node.isSamePoint(point)) {
+            return node;
+        }
+
+        if (node.compareTo(point) > 0) {
+            return get(node.left, point);
+        }
+        else {
+            return get(node.right, point);
+        }
     }
 
     private void validateArgs(final Object... args) {
@@ -24,26 +71,15 @@ public class KdTree {
         }
     }
 
-    public boolean isEmpty() {
-        return points.isEmpty();
-    }
-
-    public int size() {
-        return points.size();
-    }
-
-    public void insert(final Point2D p) {
-        validateArgs(p);
-        points.add(p);
-    }
-
     public boolean contains(final Point2D p) {
         validateArgs(p);
-        return points.contains(p);
+        return get(root, p) != null;
     }
 
     public void draw() {
-        points.parallelStream().forEach(Point2D::draw);
+        if (root != null) {
+            root.draw();
+        }
     }
 
     public Iterable<Point2D> range(final RectHV rect) {
@@ -59,5 +95,50 @@ public class KdTree {
 
     public static void main(String[] args) {
 
+    }
+
+    private class Node implements Comparable<Point2D> {
+        private final Point2D point;
+        private final RectHV rect;
+        private final boolean isVertical;
+        private Node left = null;
+        private Node right = null;
+
+        public Node(final Point2D point, final boolean isVertical) {
+            this.point = point;
+            this.rect = null; // TODO
+            this.isVertical = isVertical;
+        }
+
+        public Color getColor() {
+            return isVertical ? StdDraw.RED : StdDraw.BLUE;
+        }
+
+        public boolean isSamePoint(final Point2D pointToCheck) {
+            return this.point.compareTo(pointToCheck) == 0;
+        }
+
+        public int compareTo(final Point2D thatPoint) {
+            if (isVertical) {
+                return Double.compare(this.point.x(), thatPoint.x());
+            }
+            else {
+                return Double.compare(this.point.y(), thatPoint.y());
+            }
+        }
+
+        public void draw() {
+            // TODO: draw lines
+            StdDraw.setPenRadius(0.01);
+            StdDraw.setPenColor(StdDraw.BLACK);
+            point.draw();
+            if (left != null) {
+                left.draw();
+            }
+
+            if (right != null) {
+                right.draw();
+            }
+        }
     }
 }

530de47313feead0d370393cd11861e97a675bee
Pavel Lazarenko <pawlaz33@gmail.com>
2024-06-15 13:03:01 +0200

    WIP WordNet hypernyms parsing

--- w1_word_net/src/WordNet.java
+++ w1_word_net/src/WordNet.java
@@ -1,12 +1,8 @@
-// TODO
-// Corner cases.  Throw an IllegalArgumentException in the following situations:
-// Any argument to the constructor or an instance method is null
-// The input to the constructor does not correspond to a rooted DAG.
-// Any of the noun arguments in distance() or sap() is not a WordNet noun.
-
+import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -14,20 +10,21 @@ import java.util.Objects;
 import java.util.Set;
 
 public class WordNet {
-    private static final String FILE_SEPARATOR = ",";
+    private static final String COLUMNS_SEPARATOR = ",";
     private static final String NOUNS_SEPARATOR = " ";
-    private final Map<Integer, String> synsets;
+    private final Map<Integer, String> synsetValues;
     private final Map<String, Set<Integer>> nouns;
-    // private final Digraph digraph;
+    private final Digraph digraph;
 
     public WordNet(final String synsetsPath, final String hypernymsPath) {
         validateArguments(synsetsPath, hypernymsPath);
 
-        this.synsets = new HashMap<>();
+        this.synsetValues = new HashMap<>();
         this.nouns = new HashMap<>();
-
         parseSynsetsFile(synsetsPath);
 
+        this.digraph = new Digraph(synsetValues.size());
+        parseHypernymsFile(hypernymsPath);
     }
 
     private void validateArguments(final Object... args) {
@@ -40,11 +37,11 @@ public class WordNet {
     private void parseSynsetsFile(final String synsetsPath) {
         In reader = new In(synsetsPath);
         while (reader.hasNextLine()) {
-            String[] synsetDefinition = reader.readLine().split(FILE_SEPARATOR);
+            String[] synsetDefinition = reader.readLine().split(COLUMNS_SEPARATOR);
             int synsetId = Integer.parseInt(synsetDefinition[0]);
             String[] synsetNouns = synsetDefinition[1].split(NOUNS_SEPARATOR);
 
-            synsets.put(synsetId, synsetDefinition[1]);
+            synsetValues.put(synsetId, synsetDefinition[1]);
             for (String noun : synsetNouns) {
                 Set<Integer> ids = nouns.computeIfAbsent(noun, s -> new HashSet<>());
                 ids.add(synsetId);
@@ -52,29 +49,43 @@ public class WordNet {
         }
     }
 
-    // returns all WordNet nouns
-    public Iterable<String> nouns() {
-        // topological sort
+    private void parseHypernymsFile(final String hypernymsPath) {
+        In reader = new In(hypernymsPath);
+        while (reader.hasNextLine()) {
+            String[] hypernymsDefinition = reader.readLine().split(COLUMNS_SEPARATOR);
+            int synsetId = Integer.parseInt(hypernymsDefinition[0]);
+
+            for (int i = 1; i < hypernymsDefinition.length; i++) {
+                int hypId = Integer.parseInt(hypernymsDefinition[i]);
+                digraph.addEdge(synsetId, hypId);
+            }
+        }
     }
 
-    // is the word a WordNet noun?
-    public boolean isNoun(final String word) {
+    public Iterable<String> nouns() {
+        return Collections.unmodifiableSet(nouns.keySet());
+    }
 
+    public boolean isNoun(final String word) {
+        return nouns.containsKey(word);
     }
 
     // distance between nounA and nounB (defined below)
     public int distance(final String nounA, final String nounB) {
-
+        return -1; // TODO
     }
 
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below)
     public String sap(final String nounA, final String nounB) {
-
+        return ""; // TODO
     }
 
     // do unit testing of this class
     public static void main(String[] args) {
-
+        String synsetsPath = "./resources/wordnet/synsets15.txt";
+        String hypernymsPath = "./resources/wordnet/hypernyms15Path.txt";
+        WordNet wordNet = new WordNet(synsetsPath, hypernymsPath);
+        wordNet.isNoun("test");
     }
 }

e65323c71efe125897b94b67dfd4335ecfa100db
Pavel Lazarenko <pawlaz33@gmail.com>
2024-06-22 18:08:52 +0200

    seam search added

--- w2_seam_carving/src/SeamCarver.java
+++ w2_seam_carving/src/SeamCarver.java
@@ -1,10 +1,12 @@
 import edu.princeton.cs.algs4.Picture;
 
 import java.awt.Color;
+import java.util.Arrays;
 
 public class SeamCarver {
     private static final double BORDER_PIXEL_ENERGY = 1000;
     private final Picture p;
+    private int[][] edgeTo;
 
     public SeamCarver(final Picture picture) {
         if (picture == null) {
@@ -49,12 +51,93 @@ public class SeamCarver {
         return Math.sqrt(calcGradient(right, left) + calcGradient(top, bottom));
     }
 
+    private void relax(
+            int prevIndex, int currentIndex,
+            double energy, int x, int y,
+            double[] distTo, double[] prevDistTo
+    ) {
+        if (prevIndex < 0 || prevIndex >= prevDistTo.length) {
+            return;
+        }
+
+        double weight = prevDistTo[prevIndex];
+        if (distTo[currentIndex] > weight + energy) {
+            distTo[currentIndex] = weight + energy;
+            edgeTo[x][y] = prevIndex;
+        }
+    }
+
+    private int findIndexWithMinWeight(double[] distTo) {
+        // coordinate with min weight will be a seam end
+        double minWeight = Double.POSITIVE_INFINITY;
+        int min = 0;
+
+        for (int i = 0; i < distTo.length; i++) {
+            double weight = distTo[i];
+            if (weight < minWeight) {
+                min = i;
+                minWeight = weight;
+            }
+        }
+
+        return min; // x for vertical, y for horizontal
+    }
+
     public int[] findHorizontalSeam() {
-        return new int[0];
+        this.edgeTo = new int[this.width()][this.height()];
+        double[] distTo = new double[this.height()];
+
+        Arrays.fill(distTo, BORDER_PIXEL_ENERGY);
+        for (int x = 1; x < this.width(); x++) {
+            double[] prevDistTo = Arrays.copyOf(distTo, distTo.length);
+            Arrays.fill(distTo, Double.POSITIVE_INFINITY);
+
+            for (int y = 1; y < this.height(); y++) {
+                double energy = energy(x, y);
+
+                relax(y - 1, y, energy, x, y, distTo, prevDistTo);
+                relax(y, y, energy, x, y, distTo, prevDistTo);
+                relax(y + 1, y, energy, x, y, distTo, prevDistTo);
+            }
+        }
+
+
+        int min = findIndexWithMinWeight(distTo);
+        int[] seam = new int[this.width()];
+        for (int x = this.width() - 1; x >= 0; x--) {
+            seam[x] = min;
+            min = edgeTo[x][min];
+        }
+
+        return seam;
     }
 
     public int[] findVerticalSeam() {
-        return new int[0];
+        this.edgeTo = new int[this.width()][this.height()];
+        double[] distTo = new double[this.width()];
+
+        Arrays.fill(distTo, BORDER_PIXEL_ENERGY);
+        for (int y = 1; y < this.height(); y++) {
+            double[] prevDistTo = Arrays.copyOf(distTo, distTo.length);
+            Arrays.fill(distTo, Double.POSITIVE_INFINITY);
+
+            for (int x = 1; x < this.width(); x++) {
+                double energy = energy(x, y);
+
+                relax(x - 1, x, energy, x, y, distTo, prevDistTo);
+                relax(x, x, energy, x, y, distTo, prevDistTo);
+                relax(x + 1, x, energy, x, y, distTo, prevDistTo);
+            }
+        }
+
+        int min = findIndexWithMinWeight(distTo);
+        int[] seam = new int[this.height()];
+        for (int y = this.height() - 1; y >= 0; y--) {
+            seam[y] = min;
+            min = edgeTo[min][y];
+        }
+
+        return seam;
     }
 
     private void validateSeam(int[] seam) {

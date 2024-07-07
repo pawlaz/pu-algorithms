bd97c62726bf1929325aa6715c36cee1b51d7577
Pavel Lazarenko <pawlaz33@gmail.com>
2024-07-07 19:09:06 +0200

    boggle init

---
+++ w4_boggle/src/BoggleSolver.java
@@ -0,0 +1,13 @@
+public class BoggleSolver {
+    public BoggleSolver(String[] dictionary) {
+
+    }
+
+    public Iterable<String> getAllValidWords(final BoggleBoard board) {
+        return null;
+    }
+
+    public int scoreOf(final String word) {
+        return -1;
+    }
+}

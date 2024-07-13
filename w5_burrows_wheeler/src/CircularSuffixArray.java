354bc599c0321896ee6ac80a64da185ff7600a62
Pavel Lazarenko <pawlaz33@gmail.com>
2024-07-13 14:15:55 +0200

    CircularSuffixArray added

--- w5_burrows_wheeler/src/CircularSuffixArray.java
+++ w5_burrows_wheeler/src/CircularSuffixArray.java
@@ -1,21 +1,52 @@
-public class CircularSuffixArray {
-    // circular suffix array of s
-    public CircularSuffixArray(String s) {
+import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Arrays;
+
+public class CircularSuffixArray {
+    private final Integer[] indices;
+
+    // circular suffix array of s
+    public CircularSuffixArray(final String s) {
+        if (s == null) {
+            throw new IllegalArgumentException("String can't be null");
+        }
+        this.indices = new Integer[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            indices[i] = i;
+        }
+
+        Arrays.sort(indices, (i1, i2) -> {
+            for (int i = 0; i < s.length(); i++) {
+                int index1 = (i1 + i) % s.length();
+                int index2 = (i2 + i) % s.length();
+                int result = s.charAt(index1) - s.charAt(index2);
+                if (result != 0) {
+                    return result;
+                }
+            }
+            return 0;
+        });
     }
 
     // length of s
     public int length() {
-        return -1;
+        return indices.length;
     }
 
     // returns index of ith sorted suffix
     public int index(int i) {
-        return -1;
+        if (i < 0 || i >= this.indices.length) {
+            throw new IllegalArgumentException("Incorrect index: " + i);
+        }
+        return this.indices[i];
     }
 
     // unit testing (required)
     public static void main(String[] args) {
-
+        CircularSuffixArray csa = new CircularSuffixArray(args[0]);
+        for (int i = 0; i < csa.length(); i++) {
+            StdOut.print(csa.index(i) + " ");
+        }
+        StdOut.println("");
     }
 }
